95daef0bcb2b8b6ed04fd6bc1a01e423c50ba4fa
Sanjay9745 <sanjaysanthosh919@gmail.com>
2023-12-15 11:06:10 +0530

    finished

--- frontend/src/config/SERVER_URL.js
+++ frontend/src/config/SERVER_URL.js
@@ -1,6 +1,6 @@
 // const SERVER_URL = "http://localhost:3000/api"
 // const SERVER_URL = "https://intuc.onrender.com/api"
- const SERVER_URL = "https://backend.intucthrissur.com"
+ const SERVER_URL = "https://backend.intucthrissur.com/api"
 
 
 export default SERVER_URL;
